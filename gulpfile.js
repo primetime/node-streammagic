117c3d824d836d698ac15691b8f65301dfb90cf8
Wouter Buckens <wouter.buckens@inxco.be>
2016-12-06 15:01:45 +0100

    Simplified gulpfile - removed direct coffee-script dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
-// Require coffeescript
-require('coffee-script/register');
-// Run coffeescript gulpfile
-require('./gulpfile.coffee');
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var plumber = require('gulp-plumber');
+
+// Compiles coffeescript to javascript
+gulp.task('compile', function(){
+	gulp.src('./src/*.coffee')
+		.pipe(plumber())
+		.pipe(coffee({bare: true}))
+		.pipe(gulp.dest('./'));
+});
+
+// Watches for file changes
+gulp.task('default', ['compile'], function(){
+	gulp.watch('./src/*.coffee', ['compile']);
+});
