ccfb1aa35bf5946840e6b778c6fccdfef6887f9f
Wouter Buckens <wou.buc@gmail.com>
2017-03-30 15:37:41 +0200

    Added sourcemaps for code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 
 const coffee = require('gulp-coffee');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 // Compiles coffeescript to javascript
@@ -12,6 +13,15 @@ gulp.task('compile', function(){
 		.pipe(gulp.dest('./dist'));
 });
 
+gulp.task('compile-sourcemaps', function(){
+	gulp.src('./src/*.coffee')
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(coffee({bare: true}))
+		.pipe(sourcemaps.mapSources(function(sourcePath){ return '../src/' + sourcePath; }))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./dist'));
+})
 
 // Watches for file changes
 gulp.task('default', ['compile'], function(){
